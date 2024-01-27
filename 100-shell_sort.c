c66aa974483263fb8aa1c8d805b8c9857f106a86
Erdey <erdeysyoum@gmail.com>
2024-01-27 01:42:16 -0800

    sorting using shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sorts an array using a shell sort
+ * @array: array of integers to be sorted
+ * @size: size of an array
+ */
+void shell_sort(int *array, size_t size)
+{
+	int i, j, gap, n, Knuth_max, temp;
+	
+	if (!array || size < 2)
+		return;
+	n = (int)size;
+	for (gap = 1; gap < n; gap = (gap * 3) + 1)
+	{
+		Knuth_max = gap;
+	}
+	for (gap = Knuth_max; gap > 0; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < n; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
